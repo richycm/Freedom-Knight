0173df49d35b77612ec7908455910aef95669ced
Ricardo Carmona Martínez <ricardocarmonamartinez1iv12@gmail.com>
2026-06-12 19:08:13 -0600

    Icon
    The Freedom Knight icon was added, and we now have the official version fully functional.

--- freedom-knight/FreedomKnight1.1.service.worker.js
+++ freedom-knight/FreedomKnight1.1.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780609656|229591557';
+const CACHE_VERSION = '1781312565|2968975387';
 /** @type {string} */
 const CACHE_PREFIX = 'Freedom Knight-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
